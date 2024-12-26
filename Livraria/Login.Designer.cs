d475c0d688acf8e69fac4b1c1cc8b74b922b57fa
Carlos Daniel <carlos.oficial.ti@gmail.com>
2024-12-26 15:15:27 -0300

    Criação do projeto, Livraria !

--- Livraria/Login.Designer.cs
+++ Livraria/Login.Designer.cs
@@ -23,10 +23,16 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(734, 491);
+            Name = "Login";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
